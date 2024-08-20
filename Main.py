29eb55f49ff46bac477f0595d62e6c0447a053eb
Sharkjr17 <120116657+Sharkjr17@users.noreply.github.com>
2024-08-20 19:00:13 +0000

    modified:   Main.py

--- Main.py
+++ Main.py
@@ -6,19 +6,7 @@ import array
 #create le funny printing function
 def scanPrint(str):
 
-    step = 0
-    inputText = str
-    length = len(inputText)
 
-    while(step < length):
-
-        print(inputText[0: step + 1], end = "\r")
-        time.sleep(0.02)
-        step = step + 1
-
-    print("\r")
-
-scanPrint("Penis")
 Board = ["X", 
          "-", "-", "-", "-", "-", "-", "-", "-", 
          "-", "-", "-", "-", "-", "-", "-", "-", 
