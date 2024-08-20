f4d981f3bf9af443854f4309481e157cca79b8a7
Sharkjr17 <120116657+Sharkjr17@users.noreply.github.com>
2024-08-20 16:18:40 +0000

    new file:   Main.py 	deleted:    mainChess.py

---
+++ Main.py
@@ -0,0 +1,53 @@
+import time
+import random
+import array
+
+# ♔	 ♕	♖	♗	♘	♙	♚	♛	♜	♝	♞	♟
+#create le funny printing function
+def scanPrint(str):
+
+    step = 0
+    inputText = str
+    length = len(inputText)
+
+    while(step < length):
+
+        print(inputText[0: step + 1], end = "\r")
+        time.sleep(0.02)
+        step = step + 1
+
+    print("\r")
+
+scanPrint("Penis")
+Board = ["X", 
+         "-", "-", "-", "-", "-", "-", "-", "-", 
+         "-", "-", "-", "-", "-", "-", "-", "-", 
+         "-", "-", "-", "-", "-", "-", "-", "-", 
+         "-", "-", "-", "-", "-", "-", "-", "-", 
+         "-", "-", "-", "-", "-", "-", "-", "-", 
+         "-", "-", "-", "-", "-", "-", "-", "-", 
+         "-", "-", "-", "-", "-", "-", "-", "-", 
+         "-", "-", "-", "-", "-", "-", "-", "-"]
+
+def printBoard():
+
+    #Row A
+    print(Board[1:9])
+    #Row B
+    print(Board[9:17])
+    #Row C
+    print(Board[17:25])
+    #Row D
+    print(Board[25:33])
+    #Row E
+    print(Board[33:41])
+    #Row F
+    print(Board[41:49])
+    #Row G
+    print(Board[49:57])
+    #Row H
+    print(Board[57:65])
+
+printBoard()
+
+
