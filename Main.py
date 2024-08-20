6e6fb079e0ae49705e3a46cb2c008d9b40a700e8
Sharkjr17 <120116657+Sharkjr17@users.noreply.github.com>
2024-08-20 21:06:40 +0000

    modified:   Main.py 	renamed:    mainChess.py -> Putis.py

--- Main.py
+++ Main.py
@@ -1,15 +1,14 @@
 import time
 import random
 import array
-
-# ♔	 ♕	♖	♗	♘	♙	♚	♛	♜	♝	♞	♟
-#create le funny printing function
-def scanPrint(str):
+from colorama import Fore
 
 
-Board = ["X", 
+
+
+Board = [
          "-", "-", "-", "-", "-", "-", "-", "-", 
-         "-", "-", "-", "-", "-", "-", "-", "-", 
+         "P", "P", "P", "P", "-", "-", "-", "-", 
          "-", "-", "-", "-", "-", "-", "-", "-", 
          "-", "-", "-", "-", "-", "-", "-", "-", 
          "-", "-", "-", "-", "-", "-", "-", "-", 
@@ -20,22 +19,25 @@ Board = ["X",
 def printBoard():
 
     #Row A
-    print(Board[1:9])
+    print(Board[0:8])
     #Row B
-    print(Board[9:17])
+    print(Board[8:16])
     #Row C
-    print(Board[17:25])
+    print(Board[16:24])
     #Row D
-    print(Board[25:33])
+    print(Board[24:32])
     #Row E
-    print(Board[33:41])
+    print(Board[32:40])
     #Row F
-    print(Board[41:49])
+    print(Board[40:48])
     #Row G
-    print(Board[49:57])
+    print(Board[48:56])
     #Row H
-    print(Board[57:65])
+    print(Board[56:64])
 
+    for i in range(len(Board)):
+            print(f"{Fore.RED}" + Board[i], end = " ")
 printBoard()
 
-
+print(f"{Fore.RED}This text is red!{Fore.WHITE}")
+print(14%7)
