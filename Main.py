e69a0589f7a59997822615e6f2548dbc5d9fc367
Bananoker <169085955+Bananoker@users.noreply.github.com>
2024-08-21 14:31:26 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,43 +1,77 @@
 import time
 import random
 import array
-from colorama import Fore
 
-
-
-
+#Create vital variables
 Board = [
-         "-", "-", "-", "-", "-", "-", "-", "-", 
-         "P", "P", "P", "P", "-", "-", "-", "-", 
-         "-", "-", "-", "-", "-", "-", "-", "-", 
-         "-", "-", "-", "-", "-", "-", "-", "-", 
+         "R", "N", "B", "Q", "K", "B", "N", "R", 
+         "P", "P", "P", "P", "P", "P", "P", "P", 
          "-", "-", "-", "-", "-", "-", "-", "-", 
          "-", "-", "-", "-", "-", "-", "-", "-", 
          "-", "-", "-", "-", "-", "-", "-", "-", 
-         "-", "-", "-", "-", "-", "-", "-", "-"]
+         "-", "-", "-", "-", "-", "-", "-", "-", 
+         "p", "p", "p", "p", "p", "p", "p", "p", 
+         "r", "n", "b", "q", "k", "b", "n", "r"]
+currentTurn = 1
+currentPlayer = "Lower case"
 
+#Defines function that prints text awesome style for cooler text
+def scanPrint(str):
+  step = 0
+  inputText = str
+  length = len(inputText)
+  
+  while(step < length):
+    print(inputText[0: step + 1], end = "\r")
+
+    time.sleep(0.02)
+
+    step = step + 1
+
+  print("\r");
+
+#Prints the board for the player to see
 def printBoard():
 
-    #Row A
-    print(Board[0:8])
-    #Row B
-    print(Board[8:16])
-    #Row C
-    print(Board[16:24])
-    #Row D
-    print(Board[24:32])
-    #Row E
-    print(Board[32:40])
-    #Row F
-    print(Board[40:48])
-    #Row G
-    print(Board[48:56])
-    #Row H
-    print(Board[56:64])
+    #Column labels
+    print("     a   b   c   d   e   f   g   h")
+    #Top buffer
+    print(" ==================================")
 
-    for i in range(len(Board)):
-            print(f"{Fore.RED}" + Board[i], end = " ")
-printBoard()
+    #Row prints include row labels from 8 to 1, top-down
 
-print(f"{Fore.RED}This text is red!{Fore.WHITE}")
-print(14%7)
+    #Row 8
+    print("8|  ",Board[0]," ",Board[1]," ",Board[2]," ",Board[3]," ",Board[4]," ",Board[5]," ",Board[6]," ",Board[7])
+    #Row 7
+    print("7|  ",Board[8]," ",Board[9]," ",Board[10]," ",Board[11]," ",Board[12]," ",Board[13]," ",Board[14]," ",Board[15])
+    #Row 6
+    print("6|  ",Board[16]," ",Board[17]," ",Board[18]," ",Board[19]," ",Board[20]," ",Board[21]," ",Board[22]," ",Board[23])
+    #Row 5
+    print("5|  ",Board[24]," ",Board[25]," ",Board[26]," ",Board[27]," ",Board[28]," ",Board[29]," ",Board[30]," ",Board[31])
+    #Row 4
+    print("4|  ",Board[32]," ",Board[33]," ",Board[34]," ",Board[35]," ",Board[36]," ",Board[37]," ",Board[38]," ",Board[39])
+    #Row 3
+    print("3|  ",Board[40]," ",Board[41]," ",Board[42]," ",Board[43]," ",Board[44]," ",Board[45]," ",Board[46]," ",Board[47])
+    #Row 2
+    print("2|  ",Board[48]," ",Board[49]," ",Board[50]," ",Board[51]," ",Board[52]," ",Board[53]," ",Board[54]," ",Board[55])
+    #Row 1
+    print("1|  ",Board[56]," ",Board[57]," ",Board[58]," ",Board[59]," ",Board[60]," ",Board[61]," ",Board[62]," ",Board[63])
+
+def turn():
+    print()
+    
+def gameStart():
+    scanPrint("Welcome to ultimate chess!")
+    scanPrint("==========================")
+    scanPrint("Would you like a quick guide?")
+    print()
+    guide = input("Yes or no: ")
+
+    guide = guide.upper()
+
+    if((guide == "YES") or (guide == "Y")):
+       pass
+
+    turn()
+
+gameStart()
