ac447734d596be382df373779ddd42bdbaf84f44
Sharkjr17 <120116657+Sharkjr17@users.noreply.github.com>
2024-08-26 16:13:11 +0000

    modified:   BoardPrinter.py 	modified:   Main.py 	deleted:    Turn.py

--- BoardPrinter.py
+++ BoardPrinter.py
@@ -3,17 +3,21 @@ import random
 import array
 import sys, subprocess
 import math
+import Extras
+
+currentTurn = 1
+currentPlayer = "Lowercase"
 
 
 Board = [
-         "1", "N", "B", "Q", "K", "B", "N", "8", 
+         "R", "N", "B", "Q", "K", "B", "N", "R", 
          "P", "P", "P", "P", "P", "P", "P", "P", 
          "-", "-", "-", "-", "-", "-", "-", "-", 
          "-", "-", "-", "-", "-", "-", "-", "-", 
          "-", "-", "-", "-", "-", "-", "-", "-",
          "-", "-", "-", "-", "-", "-", "-", "-", 
          "p", "p", "p", "p", "p", "p", "p", "p", 
-         "9", "n", "b", "q", "k", "b", "n", "1"]
+         "r", "n", "b", "q", "k", "b", "n", "r"]
 
 
 #Prints the board for the player to see
@@ -43,3 +47,105 @@ def printBoard():
     print("2|  ",Board[48]," ",Board[49]," ",Board[50]," ",Board[51]," ",Board[52]," ",Board[53]," ",Board[54]," ",Board[55])
     #Row 1
     print("1|  ",Board[56]," ",Board[57]," ",Board[58]," ",Board[59]," ",Board[60]," ",Board[61]," ",Board[62]," ",Board[63])
+
+
+
+
+
+
+
+
+
+
+def turn():
+    print()
+    if currentTurn % 2 == 0:
+      currentPlayer = "Uppercase"
+    if currentTurn % 2 == 1:
+      currentPlayer = "Lowercase"
+    #Print out what turn it is
+    print("Turn: " + str(currentTurn))
+    print("Player: " + currentPlayer)
+    print()
+
+    #Print out the current board
+    print("Current Board:")
+    printBoard()
+    print()
+
+    #Begin player input
+    print("Choose a piece position:")
+    print()
+    i = input().upper
+    chosenPosition = 0
+
+    #Extremely stupid way to find chosen position
+
+    #Column:
+    if(i(1) == "1"):
+      chosenPosition = -1
+    elif(i(1) == "2"):
+      chosenPosition = 7
+    elif(i(1) == "3"):
+      chosenPosition = 15
+    elif(i(1) == "4"):
+      chosenPosition == 23
+    elif(i(1) == "5"):
+      chosenPosition == 31
+    elif(i(1) == "6"):
+      chosenPosition == 39
+    elif(i(1) == "7"):
+      chosenPosition == 47
+    elif(i(1) == "8"):
+      chosenPosition == 55
+    else:
+      #If the player sucks at putting in numbers
+      print("Invalid input! Try again loser!")
+      turn()
+
+    #Convert row to integer
+    if(i(0) == "A"):
+      rowVal = 1
+    elif(i(0) == "B"):
+      rowVal = 1
+    elif(i(0) == "C"):
+      rowVal = 1
+    elif(i(0) == "D"):
+      rowVal = 1
+    elif(i(0) == "E"):
+      rowVal = 1
+    elif(i(0) == "F"):
+      rowVal = 1
+    elif(i(0) == "G"):
+      rowVal = 1
+    elif(i(0) == "H"):
+      rowVal = 1
+    else:
+      #If the player sucks at putting in letters
+      print("Invalid input! Try again loser!")
+      turn()
+
+    #Put the row into it baby
+    chosenPosition = chosenPosition + i(1)
+
+    
+
+
+def positionScan(int):
+  chosenPos = Board[int]
+
+  #What the hell is there?
+  if(chosenPos == '-'):
+    displayedPiece = "nothing"
+  elif((chosenPos == 'P') or (chosenPos == "P")):
+    displayedPiece = "a pawn"
+  elif((chosenPos == "r") or (chosenPos == "R")):
+    displayedPiece = "a rook"
+  elif((chosenPos == "n") or (chosenPos == "N")):
+    displayedPiece = "a knight"
+  elif((chosenPos == "b") or ()
+    
+
+
+
+  print("Chosen position is occupied by ")
--- Main.py
+++ Main.py
@@ -6,14 +6,6 @@ import math
 import BoardPrinter
 import Extras
 
-#Create vital variables
-
-currentTurn = 1
-currentPlayer = "Lowercase"
-
-
-
-
 
 
 def guide():
@@ -40,107 +32,11 @@ def guide():
     
 
 
-def turn():
-    print()
-    if currentTurn % 2 == 0:
-      currentPlayer = "Uppercase"
-    if currentTurn % 2 == 1:
-      currentPlayer = "Lowercase"
-    #Print out what turn it is
-    print("Turn: " + str(currentTurn))
-    print("Player: " + currentPlayer)
-    print()
-
-    #Print out the current board
-    print("Current Board:")
-    BoardPrinter.printBoard()
-    print()
-
-    #Begin player input
-    print("Choose a piece position:")
-    print()
-    i = input().upper
-    chosenPosition = 0
-
-    #Extremely stupid way to find chosen position
-
-    #Column:
-    if(i(1) == "1"):
-      chosenPosition = -1
-    elif(i(1) == "2"):
-      chosenPosition = 7
-    elif(i(1) == "3"):
-      chosenPosition = 15
-    elif(i(1) == "4"):
-      chosenPosition == 23
-    elif(i(1) == "5"):
-      chosenPosition == 31
-    elif(i(1) == "6"):
-      chosenPosition == 39
-    elif(i(1) == "7"):
-      chosenPosition == 47
-    elif(i(1) == "8"):
-      chosenPosition == 55
-    else:
-      #If the player sucks at putting in numbers
-      print("Invalid input! Try again loser!")
-      turn()
-
-    #Convert row to integer
-    if(i(0) == "A"):
-      rowVal = 1
-    elif(i(0) == "B"):
-      rowVal = 1
-    elif(i(0) == "C"):
-      rowVal = 1
-    elif(i(0) == "D"):
-      rowVal = 1
-    elif(i(0) == "E"):
-      rowVal = 1
-    elif(i(0) == "F"):
-      rowVal = 1
-    elif(i(0) == "G"):
-      rowVal = 1
-    elif(i(0) == "H"):
-      rowVal = 1
-      
-    if(i(0) == "1"):
-      chosenPosition = -1
-    elif(i(0) == "2"):
-      chosenPosition = 7
-    elif(i(0) == "C"):
-      chosenPosition = 15
-    elif(i(0) == "D"):
-      chosenPosition == 23
-    elif(i(0) == "E"):
-      chosenPosition == 31
-    elif(i(0) == "F"):
-      chosenPosition == 39
-    elif(i(0) == "G"):
-      chosenPosition == 47
-    elif(i(0) == "H"):
-      chosenPosition == 55
-    else:
-      #If the player sucks at putting in letters
-      print("Invalid input! Try again loser!")
-      turn()
-
-    #Put the row into it baby
-    chosenPosition = chosenPosition + i(1)
-
-    
-
-
-def positionScan(int):
-  boardPos = BoardPrinter.board[int]
-
-
-
 #Run The Game
 def gameStart():
     guide()
 
-    turn()
+    BoardPrinter.turn()
 
 #Run The Script (To Run The Game!)
 subprocess.run('clear', shell=True)
